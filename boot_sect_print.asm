698364725113c4bb0d5eb6443ab7ca3f11341fc5
Nikita Skrypchenko <frickadelka456@gmail.com>
2019-02-10 12:49:08 +0200

    Hello, Greedy

---
+++ boot_sect_print.asm
@@ -0,0 +1,33 @@
+;simple print on the screen and move the cursor
+
+mov ah, 0x0e ;scrolling teletype BIOS routine
+
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, ','
+int 0x10
+mov al, ' '
+int 0x10
+mov al, 'G'
+int 0x10
+mov al, 'r'
+int 0x10
+mov al, 'e'
+int 0x10
+int 0x10
+mov al, 'd'
+int 0x10
+mov al, 'y'
+int 0x10
+
+;padding
+jmp $
+times 510 - ($-$$) db 0
+dw 0xaa55
