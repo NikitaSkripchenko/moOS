425fb091c8a08c5ed2d2ccfd755ae085bdb81bf0
Nikita Skrypchenko <frickadelka456@gmail.com>
2019-02-19 10:34:41 +0200

    add 32printf

--- 32bit_printf.asm
+++ 32bit_printf.asm
@@ -1,26 +1,30 @@
-[bits 32] ; using 32-bit protected mode
+; The formula for accessing a specific character on the 80x25 grid is:0xb8000 + 2 * (row * 80 + col)
 
-; this is how constants are defined
+[bits 32]
+; Define some constants 
 VIDEO_MEMORY equ 0xb8000
-WHITE_ON_BLACK equ 0x0f ; the color byte for each character
-
+WHITE_ON_BLACK equ 0x0f
+; prints a null-terminated string pointed to by EDX
 print_string_pm:
     pusha
-    mov edx, VIDEO_MEMORY
+    mov edx, VIDEO_MEMORY ; Set edx to the start of vid mem.
 
 print_string_pm_loop:
-    mov al, [ebx] ; [ebx] is the address of our character
-    mov ah, WHITE_ON_BLACK
+    mov al, [ebx] ; Store the char at EBX in AL 
+    mov ah, WHITE_ON_BLACK ; Store the attributes in AH
 
-    cmp al, 0 ; check if end of string
+    cmp al, 0 
     je print_string_pm_done
 
-    mov [edx], ax ; store character + attribute in video memory
-    add ebx, 1 ; next char
-    add edx, 2 ; next video memory position
+    mov [edx], ax
+    add ebx, 1
+    add edx, 2
+; if (al == 0), at end of string, so ; jump to done
+; Store char and attributes at current
+; character cell.
+; Increment EBX to the next char in string. ; Move to next character cell in vid mem.
+    jmp print_string_pm_loop ; loop around to print the next char.
 
-    jmp print_string_pm_loop
-
-print_string_pm_done:
+print_string_pm_done :
     popa
-    ret
+    ret ; Return from the function
